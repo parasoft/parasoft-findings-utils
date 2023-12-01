67bf2917ff08c22f876f6f2a46580ff4f7239e65
Alfie Wang <awang@parasoft.com>
2023-12-01 17:19:30 +0800

    Disable unit tests of requesting remote docs if DTP URL is not specified.

--- src/test/java/com/parasoft/findings/utils/doc/RuleDocumentationProviderTest.java
+++ src/test/java/com/parasoft/findings/utils/doc/RuleDocumentationProviderTest.java
@@ -1,6 +1,7 @@
 package com.parasoft.findings.utils.doc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 
 import java.io.File;
 import java.util.Properties;
@@ -9,20 +10,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RuleDocumentationProviderTest {
 
-    private final String dtpUrl = "https://dtp.parasoftcn.com/202301SS";
+    private final String dtpUrl = System.getProperty("testDtpUrl"); // Use the same key as in maven-surefire-plugin configuration.
 
     @Test
+    @EnabledIf(value = "hasTestDtpUrlSystemProperty", disabledReason = "No testDtpUrl system property")
     void getRuleDocLocationFromDtp_normal() {
         Properties properties = new Properties();
         properties.setProperty("dtp.url", dtpUrl);
 
+        final String analyzer = "com.parasoft.xtest.cpp.analyzer.static.pattern";
+        final String ruleId = "APSC_DV-000160-a";
+
         RuleDocumentationProvider underTest = new RuleDocumentationProvider(properties);
-        String remoteRuleDocURL = underTest.getRuleDocLocation("com.parasoft.xtest.cpp.analyzer.static.pattern", "APSC_DV-000160-a");
-        assertEquals("https://dtp.parasoftcn.com/202301SS/grs/dtp/rulesdoc/com.parasoft.xtest.cpp.analyzer.static.pattern/10.6.2/zh_CN/APSC_DV-000160-a.html",
-                      remoteRuleDocURL);
+        String remoteRuleDocURL = underTest.getRuleDocLocation(analyzer, ruleId);
+
+        assertTrue(remoteRuleDocURL != null && remoteRuleDocURL.contains(analyzer) && remoteRuleDocURL.contains(ruleId));
     }
 
     @Test
+    @EnabledIf(value = "hasTestDtpUrlSystemProperty", disabledReason = "No testDtpUrl system property")
     void getRuleDocLocationFromDtp_incorrectAnalyzer() {
         Properties properties = new Properties();
         properties.setProperty("dtp.url", dtpUrl);
@@ -33,6 +39,7 @@ class RuleDocumentationProviderTest {
     }
 
     @Test
+    @EnabledIf(value = "hasTestDtpUrlSystemProperty", disabledReason = "No testDtpUrl system property")
     void getRuleDocLocationFromDtp_incorrectRuleId() {
         Properties properties = new Properties();
         properties.setProperty("dtp.url", dtpUrl);
@@ -70,5 +77,9 @@ class RuleDocumentationProviderTest {
         RuleDocumentationProvider underTest = new RuleDocumentationProvider(properties);
         String localRuleDocLocation = underTest.getRuleDocLocation(null, "APSC_DV-000160-a");
         assertEquals(new File("src/test/resources/ruledoc", "APSC_DV-000160-a.html").getAbsolutePath(), localRuleDocLocation);
+    }
+
+    boolean hasTestDtpUrlSystemProperty() {
+        return dtpUrl != null && !dtpUrl.trim().isEmpty();
     }
 }
